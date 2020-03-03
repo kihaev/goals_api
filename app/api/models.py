13402170419d8e7c3fffe68420c40d4085b68ce9
kihaev <yurakikhaiev@gmail.com>
2020-03-03 14:58:38 +0200

    Add progress validators to models

--- app/api/models.py
+++ app/api/models.py
@@ -1,13 +1,13 @@
 from django.db import models
-
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 
 
 class Goal(models.Model):
     title = models.CharField(max_length=255)
-    progress = models.FloatField()
-    aligned_to = models.ForeignKey('self', on_delete=models.CASCADE)
+    progress = models.FloatField(validators=[MinValueValidator(0), MaxValueValidator(1)])
+    aligned_to = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.title
