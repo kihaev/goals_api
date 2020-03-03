d731a8964839233dbaf3fa568f73d765222a5655
kihaev <yurakikhaiev@gmail.com>
2020-03-03 14:04:43 +0200

    Create goals model

--- app/api/models.py
+++ app/api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Goal(models.Model):
+    title = models.CharField(max_length=255)
+    progress = models.FloatField()
+    aligned_to = models.ForeignKey('self', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
