d2dc25c9dbe7667e7e6ba4148da0d6e35d2ddf31
kihaev <yurakikhaiev@gmail.com>
2020-03-03 15:10:54 +0200

    Add reference validation

--- app/api/views.py
+++ app/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import get_object_or_404
 from .models import Goal
 from .serializers import GoalSerializer
 
@@ -8,6 +9,10 @@ from .serializers import GoalSerializer
 class GoalView(ListCreateAPIView):
     queryset = Goal.objects.all()
     serializer_class = GoalSerializer
+
+    def perform_create(self, serializer):
+        goal = get_object_or_404(Goal, id=self.request.data.get('aligned_to'))
+        return serializer.save(goal=goal)
 
 
 class SingleGoalView(RetrieveUpdateDestroyAPIView):
